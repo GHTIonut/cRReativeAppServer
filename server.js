102405f88819922cab7e0dc8f4f92e8f52303ec4
GHTIonut <ghitaionutcristian96@yahoo.com>
2026-01-23 19:35:29 +0200

    Change password feature added + how changed password looks

--- server.js
+++ server.js
@@ -77,6 +77,7 @@ app.post("/accounts", async (req, res) => {
   if (accounts.some((acc) => acc.email === email)) {
     return res.status(400).json({ message: "Email already exists!" });
   }
+
   const hashedPassword = await bcrypt.hash(password, 10);
 
   const newUser = {
@@ -97,6 +98,49 @@ app.post("/accounts", async (req, res) => {
       email,
     },
   });
+});
+
+function authMiddleware(req, res, next) {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, SECRET);
+    req.user = decoded;
+    next();
+  } catch {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+}
+
+app.post("/changePassword", authMiddleware, async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+
+  const data = fs.readFileSync("accounts.json", "utf-8");
+  const accounts = JSON.parse(data);
+
+  const user = accounts.find((acc) => acc.email === req.user.email);
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  const isMatch = await bcrypt.compare(oldPassword, user.password);
+
+  if (!isMatch) {
+    return res.status(400).json({ message: "Old password is incorrect" });
+  }
+
+  const hashedNewPassword = await bcrypt.hash(newPassword, 10);
+
+  user.password = hashedNewPassword;
+
+  fs.writeFileSync("accounts.json", JSON.stringify(accounts, null, 2));
+
+  res.json({ message: "Password updated successfully" });
 });
 
 app.listen(3000, () => {
